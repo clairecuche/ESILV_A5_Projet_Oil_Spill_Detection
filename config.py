ac12098c471a3e01fd53eb52a8cfc3ec5d9ab2df
Ines756 <inesdarde0@gmail.com>
2025-12-25 15:47:57 +0100

    confi pour kaggle

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ MODEL_NAME_SEG = 'nvidia/segformer-b3-finetuned-cityscapes-1024-1024'
 # --- Paramètres d'Entraînement Yolo (comme le paper pour l'instant)---
 OUTPUT_DIR_YOLO = Path('./outputs/yolo')
 CHECKPOINT_DIR_YOLO = OUTPUT_DIR_YOLO / 'checkpoints'
-BATCH_SIZE_YOLO = 32 
+BATCH_SIZE_YOLO = 8
 NUM_EPOCHS_YOLO = 80
 LEARNING_RATE_YOLO = 6e-5 
 WEIGHT_DECAY = 0.01
